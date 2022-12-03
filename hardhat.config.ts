fdac8e82abed5801688dff8db5b1635bb3478e93
masonCalmAndCode <t100210022002@gmail.com>
2022-12-03 23:21:00 +0800

    add hack test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+require('dotenv').config();
+
+const { API_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+	solidity: { compilers: [{ version: '0.8.10' }, { version: '0.8.13' }] },
+	defaultNetwork: 'hardhat',
+	networks: {
+		hardhat: {
+			forking: {
+				url: `https://eth-mainnet.g.alchemy.com/v2/${API_KEY}`,
+				enabled: true,
+				blockNumber: 14595905,
+			},
+			allowUnlimitedContractSize: true,
+		},
+	},
 };
-
-export default config;
